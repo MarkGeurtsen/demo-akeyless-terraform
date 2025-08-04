a62b53e5d0ab6a82d83f5c132303642d358571e6
Mark Geurtsen <mark@Marks-MacBook-Pro.local>
2025-08-04 15:47:06 -0400

    add auth module

---
+++ modules/auth/main.tf
@@ -0,0 +1,23 @@
+resource "akeyless_auth_method_saml" "authentication_method" {
+    name = "terraform/auth-method"
+    unique_identifier = "email"
+    idp_metadata_xml_data = file("https://dev-18478873.okta.com/app/exkot5op6eUeTvji15d7/sso/saml/metadata")
+}
+
+resource "akeyless_role" "access_role" {
+    depends_on = [ akeyless_auth_method_saml.authentication_method ]
+    name = "terraform/access-role"
+
+    assoc_auth_method {
+        am_name = "terraform/auth-method"
+        sub_claims = {
+            groups = "developers"
+        }
+    }
+
+    rules {
+        capability = ["read", "list"]
+        path = "/*"
+        rule_type = "item-rule"
+    }
+}
